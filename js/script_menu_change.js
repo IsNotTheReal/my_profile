f22c5e9da4d78267d9e18dc68b91f132576841fb
Alexandre M <alexandre.mayo101@gmail.com>
2023-04-20 21:52:51 +0200

    my_profile

--- js/script_menu_change.js
+++ js/script_menu_change.js
@@ -1,11 +1,16 @@
 var box_menu = document.getElementById('list_elements_nav_zoom');
-var box_menu_items = document.getElementById('list_elements_nav_menu');
-var box = document.getElementById('btn_menu');
-var btn = document.getElementsByClassName('btn_nav_top');
-var list = document.getElementsByClassName('list');
+var btn_menu = document.getElementById('btn_menu');
 
-window.addEventListener('click', function(event) {
-    if (event.target != box_menu && event.target != box && event.target != box_menu_items && event.target != btn[3] && event.target != btn[4] && event.target != btn[5] && event.target != list[0] && event.target != list[1] && event.target != list[2]) {
+box_menu.addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+
+btn_menu.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+
+document.documentElement.addEventListener('click', function(event) {
+    if (event.target !== box_menu && !box_menu.contains(event.target) && event.target !== btn_menu) {
         box_menu.style.display = 'none';
     }
-});
+  });
--- js/script_menu_nav.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('#list_elements_nav');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
